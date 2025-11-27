f740e856e71cb3fd2e3e520ae758a3107c8478a2
Seung <ksh30916@naver.com>
2025-11-27 17:03:58 +0900

    docs: swagger API 문서화

--- server/src/app.js
+++ server/src/app.js
@@ -1,13 +1,22 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import fs from 'fs';
+import yaml from 'yaml';
+import swaggerUi from 'swagger-ui-express';
 import authRouter from './routes/authRouter.js';
 import userRouter from './routes/userRouter.js';
 import bordRouter from './routes/bordRouter.js';
-import commentRouter from './routes/commentRouter.js'
+import commentRouter from './routes/commentRouter.js';
 import { globalErrorHandler } from './controllers/errorController.js';
+
 dotenv.config();
 const app = express();
+
+const swaggerFile = fs.readFileSync(new URL('../swagger/swagger.yaml', import.meta.url), 'utf8');
+const swaggerDocument = yaml.parse(swaggerFile);
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use(cors());
 app.use(express.json());
